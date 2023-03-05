56242430009573379c59e1fdaafd8bde4b669f3f
Chris Krycho <hello@chriskrycho.com>
2023-03-04 21:10:28 -0700

    CLI started. 😎

--- lx/src/cli.rs
+++ lx/src/cli.rs
@@ -1,14 +1,40 @@
-use clap::{Parser, Subcommand};
+use clap::{CommandFactory, Parser, Subcommand};
+use clap_complete::{generate_to, shells::Fish};
+
+use crate::errors::LxError;
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-pub struct Args {
+#[command(author, version, about)]
+pub struct Lx {
     #[command(subcommand)]
-    pub mode: Mode,
+    pub command: Command,
 }
 
-#[derive(Subcommand, Debug)]
-pub enum Mode {
+impl Lx {
+    pub fn run(&mut self) -> Result<(), LxError> {
+        match self.command {
+            Command::UI => todo!(),
+            Command::Publish => todo!(),
+            Command::Completions => self.completions(),
+        }
+    }
+
+    fn completions(&mut self) -> Result<(), LxError> {
+        let mut config_dir = dirs::home_dir().ok_or_else(|| LxError::NoHomeDir)?;
+        config_dir.extend([".config", "fish", "completions"]);
+        let mut cmd = Self::command();
+        generate_to(Fish, &mut cmd, "lx", config_dir)
+            .map(|_| ())
+            .map_err(LxError::CompletionError)
+    }
+}
+
+#[derive(Subcommand, Debug, PartialEq)]
+pub enum Command {
+    /// Launch the web UI!
     UI,
+    /// Publish the site.
     Publish,
+    /// Give me completions for my own dang tool.
+    Completions,
 }
---
+++ lx/src/errors.rs
@@ -0,0 +1,9 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum LxError {
+    #[error("Somehow you don't have a home dir. lolwut")]
+    NoHomeDir,
+    #[error("Failed to generate completions")]
+    CompletionError(std::io::Error),
+}
