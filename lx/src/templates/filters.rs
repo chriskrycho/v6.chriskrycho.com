209d89edd02234e2434a3da108f58fe51a85877c
Chris Krycho <hello@chriskrycho.com>
2024-11-12 14:26:31 -0700

    lx: leave myself a TODO about page title filter

--- lx/src/templates/filters.rs
+++ lx/src/templates/filters.rs
@@ -5,6 +5,7 @@ pub fn add_filters(env: &mut Environment) {
    env.add_filter("page_title", page_title);
 }
 
+// TODO: support different variations from different sites via config.
 fn page_title(state: &State, page_title: &str) -> String {
    let base = "Sympolymathesy, by Chris Krycho";
    let current_page = state.name();
