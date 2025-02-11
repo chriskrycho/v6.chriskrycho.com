a1e6a6b74c05c54e1bc19229740b2962ec20d8e7
Chris Krycho <hello@chriskrycho.com>
2025-02-11 11:24:21 -0700

    build: support Render previews given its limitations
    I am changing my assumptions, after much thinking about how to wrangle
    this at some length.
    
    1. I will not try to do preview changes for `lx` on Render.
    2. I can therefore have Render always use the *latest* build of `lx`,
       rather than mucking around with specific versions.
    3. I will thus only deploy new builds of `lx` from `main`.
    4. I still need to avoid “auto-deploying”, so that I only deploy once a
       release has been created, for the cases where I have made changes to
       `lx` that might break existing templates.
    
    Net, then, I need to only trigger a deploy on `main` (while keeping the
    existing rules in place, too), and on the build script side just always
    use the latest version, not mucking around with PR versions.

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -22,41 +22,13 @@ LATEST="${RELEASES}/latest/download/lx-linux"
 OUTPUT="lx-cli"
 rm -f $OUTPUT
 
-download() {
-  local url="$1"
-  local output="$2"
-
-  echo "fetching '$url' to '$output'"
-
-  curl --location \
+echo "fetching '$LATEST' to '$OUTPUT'"
+curl --location \
     --proto '=https' --tlsv1.2 \
     --silent --show-error --fail \
-    --output "$output" \
-    "$url"
-}
-
-# Note for jj usage (read: local testing): make sure the repo is colocated!
-download_for_pr() {
-  local sha
-  sha=$(git rev-parse --short HEAD)
-
-  local pr="${RELEASES}/download/lx-${sha}/lx-linux"
-
-  local pr_result
-  pr_result=$(download "$pr" "$OUTPUT")
-
-  local pr_exit=$?;
-  echo "PR: $pr_exit $pr_result"
-
-  if [[ "$pr_exit" -ne 0 ]]; then
-    echo "falling back to latest: $LATEST"
-    download $LATEST $OUTPUT
-  fi
-}
-
-# This works regardless of whether Render understands that a given deploy hook
-# was triggered by a pull request or not.
-download_for_pr || exit $?
+    --output "$OUTPUT" \
+    "$LATEST" \
+  || exit $?
 
 chmod +x $OUTPUT
 
